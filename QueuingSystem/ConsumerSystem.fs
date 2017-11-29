a8bfbcc39bd429b37efd70daba73798972fdcf92
frannuca <frannuca@gmail.com>
2017-11-29 11:17:32 +0100

    first commit

---
+++ QueuingSystem/ConsumerSystem.fs
@@ -0,0 +1,55 @@
+﻿namespace QueuingSystem
+open System.Collections.Generic
+open System.Data
+open System.Threading.Tasks
+
+type JOBRESULT=
+    |SUCCESS
+    |FAILURE of System.Exception
+type JOBMESSAGE= {NSHARE:int;CHANNEL:AsyncReplyChannel<JOBRESULT>;TASK:System.Action}
+type MODELMESSAGE =
+| KILL
+| JOB of JOBMESSAGE
+
+
+
+type ConsumerSystem(nworkers:int, token:System.Threading.CancellationTokenSource)=
+            
+    let queue = new System.Collections.Concurrent.ConcurrentQueue<JOBMESSAGE>()
+    
+    let consumer(token:System.Threading.CancellationToken) =
+        while not(token.IsCancellationRequested) do
+            match queue.TryDequeue() with
+            |false,_ -> System.Threading.Thread.Sleep(500)
+            |true,job -> 
+                         try
+                         job.TASK.Invoke()
+                         job.CHANNEL.Reply(SUCCESS)
+                         with
+                         |ex  -> job.CHANNEL.Reply (FAILURE ex)
+    let workers=        
+                    [for i in 0 .. nworkers-1 do yield Task.Factory.StartNew(new System.Action(fun () -> consumer(token.Token)))]
+                    
+    
+    
+    let JOBQueuingSystem  =
+        MailboxProcessor.Start(fun inbox ->
+            let rec loop n =
+                async { let! msg = inbox.Receive()
+                        match msg with
+                        | KILL -> return ()
+                        | JOB(job) ->
+                            queue.Enqueue job
+                            //replyChannel.Reply(n)
+                            return! loop(n) }
+            loop 0)
+
+   
+    
+    member  self.AddJob(nshare:int,task:System.Action)=
+        JOBQueuingSystem.PostAndReply(fun replyChannel -> MODELMESSAGE.JOB {NSHARE=nshare;CHANNEL=replyChannel;TASK=task})
+
+    member self.Stop()=
+        JOBQueuingSystem.Post KILL
+        token.Cancel()
+
---
+++ QueuingSystem/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open QueuingSystem
+open System
+
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let task()=
+         System.Threading.Thread.Sleep(5000) |> ignore
+         printfn "%s" (System.Threading.Thread.CurrentThread.ManagedThreadId.ToString())
+         
+    let taskerror()=
+        System.Threading.Thread.Sleep(5000) |> ignore
+        failwith "AHA, this is an error"
+    
+    let token = new System.Threading.CancellationTokenSource()
+    let consumer = new ConsumerSystem(4,token)
+
+    let msg1 = System.Threading.Tasks.Task<JOBRESULT>.Factory
+                    .StartNew(fun () -> consumer.AddJob(4,new System.Action(task)))
+
+    let error =  System.Threading.Tasks.Task<JOBRESULT>.Factory
+                    .StartNew(fun () -> consumer.AddJob(4,new System.Action(taskerror)))
+
+    let result1 = msg1.Result
+    let resutl2 = error.Result;
+    
+    consumer.Stop()
+    //printfn "%A" r
+    0 // return an integer exit code
